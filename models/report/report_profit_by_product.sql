612ff05b9aa41942ee6586b13cfb32941f20e06b
ron@d-a-team.com <ron@d-a-team.com>
2022-07-18 06:36:30 +0000

    create source to the raw layer

--- models/report/report_profit_by_product.sql
+++ models/report/report_profit_by_product.sql
@@ -1,3 +1,8 @@
+{{
+    config(
+        materialized='table'
+    )
+}}
 select productid,productname,category,subcategory,sum(order_profit) as product_profit
  from {{ ref('stage_orders') }}
  group by productid,productname,category,subcategory
