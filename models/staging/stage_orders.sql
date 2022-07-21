8ddcce086c1ac703dfdb5b80ebb05deda87480f2
ron@d-a-team.com <ron@d-a-team.com>
2022-07-21 12:12:31 +0000

    create all the changes branch

--- models/staging/stage_orders.sql
+++ models/staging/stage_orders.sql
@@ -5,7 +5,9 @@
    -- field take from products table
  p.productname,p.category,p.subcategory,
   -- field take from customers table
- c.customername,c.segment,c.country
+ c.customername,c.segment,c.country,
+   -- field take from deleivery_team table
+   d.DELIVERY_TEAM
 
  from {{ ref('orders') }} as o
 left join 
@@ -14,3 +16,6 @@ on o.PRODUCTID=p.PRODUCTID
 left join 
 {{ ref('customer') }} as c
 on o.customerid=c.customerid
+left join
+  {{ ref('delivery_team') }} as d
+on o.shipmode=d.shipmode
